4724c220e703e693f244d59c5cf462e8501bce7a
lqchy <lachlantheslayer@gmail.com>
2022-11-16 20:21:17 +0000

    Add and partially implement CecerMCLib

--- src/main/java/me/lachy/hypixelutils/HypixelUtils.java
+++ src/main/java/me/lachy/hypixelutils/HypixelUtils.java
@@ -1,8 +1,13 @@
 package me.lachy.hypixelutils;
 
+import com.cecer1.projects.mc.cecermclib.common.CecerMCLib;
+import com.cecer1.projects.mc.cecermclib.common.modules.chatmetadata.ChatMetadataModule;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import me.lachy.hypixelutils.chat.ChatProcessor;
 import me.lachy.hypixelutils.command.RotationCommand;
 import me.lachy.hypixelutils.util.Scheduler;
-import net.minecraft.init.Blocks;
+import net.minecraft.client.Minecraft;
 import net.minecraftforge.client.ClientCommandHandler;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
@@ -15,11 +20,16 @@ public class HypixelUtils {
 
     public static final String MOD_ID = "hypixelutils";
     public static final String VERSION = "1.0";
+    public static final Gson GSON = new GsonBuilder()
+            .setPrettyPrinting()
+            .create();
 
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
         ClientCommandHandler.instance.registerCommand(new RotationCommand());
         MinecraftForge.EVENT_BUS.register(this);
+
+        CecerMCLib.get(ChatMetadataModule.class).registerProcessor(new ChatProcessor());
     }
 
     @SubscribeEvent
