cf9509aa66f4395202c186433d7a2a860c6d8db4
lqchy <lachlantheslayer@gmail.com>
2022-11-20 15:26:21 +0000

    Add checks for when to render

--- src/main/java/me/lachy/hypixelutils/modules/teamupgradedisplay/TeamUpgradeDisplayModule.java
+++ src/main/java/me/lachy/hypixelutils/modules/teamupgradedisplay/TeamUpgradeDisplayModule.java
@@ -12,7 +12,13 @@ import com.cecer1.projects.mc.cecermclib.forge.modules.rendering.context.Abstrac
 import com.cecer1.projects.mc.cecermclib.forge.modules.rendering.context.TransformCanvas;
 import com.google.common.collect.ImmutableSet;
 import me.lachy.hypixelutils.util.MinecraftColour;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.entity.EntityPlayerSP;
 import net.minecraft.client.gui.FontRenderer;
+import net.minecraft.client.gui.GuiIngame;
+import net.minecraft.scoreboard.ScoreObjective;
+import net.minecraft.scoreboard.Scoreboard;
+import net.minecraftforge.fml.relauncher.ReflectionHelper;
 
 import java.util.*;
 import java.util.stream.Collectors;
@@ -36,17 +42,40 @@ public class TeamUpgradeDisplayModule implements IModule {
     public void onModuleRegister() {
         AllModulesRegisteredCallback.EVENT.register(() -> CecerMCLib.get(ChatMetadataModule.class).registerProcessor(new TeamUpgradeChatProcessor()));
 
+        Minecraft minecraft = Minecraft.getMinecraft();
         SafeHudRenderCallback.EVENT.register(ctx -> {
             FontRenderer fontRenderer = ctx.getFontRenderer();
+
+            EntityPlayerSP player = minecraft.thePlayer;
+            ScoreObjective objective = null;
+            if (player != null) {
+                Scoreboard scoreboard = player.getWorldScoreboard();
+                if (scoreboard != null) {
+                    objective = scoreboard.getObjectiveInDisplaySlot(1);
+                    if (objective == null) {
+                        return;
+                    }
+                    if (objective.getName().equals("PreScoreboard")) {
+                        this.clearUpgrades();
+                        return;
+                    }
+                }
+            }
+
+            if (this.getCurrentTitle().equals("VICTORY!")) {
+                this.clearUpgrades();
+                return;
+            }
 
             AbstractCanvas canvas = ctx.getCanvas();
             try (TransformCanvas canvas1 = canvas.transform()
                     .translate(0, 0)
                     .openTransformation()) {
 
-                if (this.upgrades.isEmpty()) {
+                if (this.upgrades.isEmpty() || objective == null || !objective.getName().equals("BForeboard")) {
                     return;
                 }
+
                 fontRenderer.drawStringWithShadow("\u00a7nPurchased Upgrades:", 5, 5, MinecraftColour.GREEN.getARGB());
 
                 int y = fontRenderer.FONT_HEIGHT + 8;
@@ -67,6 +96,13 @@ public class TeamUpgradeDisplayModule implements IModule {
                 }
             }
         });
+    }
+
+    public String getCurrentTitle() {
+        try {
+            return (String) ReflectionHelper.findField(GuiIngame.class, "displayedTitle", "field_175201_x").get(Minecraft.getMinecraft().ingameGUI);
+        } catch (IllegalAccessException ignored) {}
+        return "";
     }
 
     public void addUpgrade(TeamUpgrade upgrade, String purchaser) {
